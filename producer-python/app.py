6fc45bbd64c66eb84da530fbb946c70350b34602
Kevin <kevin_mendez30@outlook.com>
2022-01-06 21:45:09 -0600

    feat: 🎸 (flask) connect flask to test stream

--- producer-python/app.py
+++ producer-python/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+from kafka import KafkaConsumer, KafkaProducer
 app = Flask(__name__)
+TOPIC_NAME = "test"
+KAFKA_SERVER = "localhost:9092"
 
-@app.route('/')
+producer = KafkaProducer(
+    bootstrap_servers = KAFKA_SERVER,
+    api_version = (0, 11, 15)
+)
+
+@app.route('/', methods=['GET'])
 def index():
     response = {'message': 'success'}
+    response = {'language': 'python', 'is': 'awesome'}
+
+    json_payload = json.dumps(response)
+    json_payload = str.encode(json_payload)
+
+    print(json_payload)
+
+    # push data into INFERENCE TOPIC
+    producer.send(TOPIC_NAME, json_payload)
+    producer.flush()
+    print("Sent to consumer")
     return jsonify(response)
 
-if __name__ == '__main__':
-  app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port = 5000)
