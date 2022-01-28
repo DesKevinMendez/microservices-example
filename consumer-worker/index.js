1b29ca11943c280962d8bf86f734f489a11c7a9e
Kevin <kevin_mendez30@outlook.com>
2022-01-27 20:58:54 -0600

    refactor: 💡 (consumer) removed innecesary packages

--- consumer-worker/index.js
+++ consumer-worker/index.js
@@ -1,18 +1,27 @@
 import Kafka from 'node-rdkafka';
 
-const consumer = new Kafka.KafkaConsumer({
-  'group.id': 'kafka',
-  'metadata.broker.list': 'localhost:9093',
-}, {});
+(() => {
+  const consumer = new Kafka.KafkaConsumer(
+    {
+      'group.id': 'kafka',
+      'metadata.broker.list': 'kafka:9092'
+    },
+    {}
+  );
 
-consumer.connect();
-console.log('Se ejecuta');
+  consumer.connect();
 
-consumer.on('ready', () => {
-  console.log('consumer ready..')
-  consumer.subscribe(['test']);
-  consumer.consume();
-}).on('data', function(data) {
-  console.log(`received message: ${data.value}`);
-  // console.log(`received message: ${objectToJson.fromBuffer(data.value)}`);
-});
+  consumer
+    .on('ready', () => {
+      console.log('consumer ready..');
+      consumer.subscribe(['test']);
+      consumer.consume();
+    })
+    .on('data', function (data) {
+      console.log(`received message: ${data.value}`);
+      // console.log(`received message: ${objectToJson.fromBuffer(data.value)}`);
+    })
+    .on('error', function (err) {
+      console.log(err);
+    });
+})();
