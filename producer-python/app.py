04f749eff3646e70f786bb15988ad9470e82c009
Kevin <kevin_mendez30@outlook.com>
2022-02-02 23:07:51 -0600

    feat: 🎸 (CRUD) for user

--- producer-python/app.py
+++ producer-python/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, jsonify
 import json
 from kafka import KafkaProducer
-from application.models import db, migrate
+from application.models import db, migrate, User
 from dotenv import load_dotenv
+from sqlalchemy import exc
 import os
 load_dotenv()
 
@@ -34,7 +35,35 @@ migrate.init_app(app, db)
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'hola'
+    return 'hello world'
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    if request.method == 'POST':
+        email = request.form['email']
+        if email:
+            try:
+                User.add_user(email)
+                return Response("User added", 201, mimetype='application/json')
+            except exc.IntegrityError:
+                return Response('User with email %s already exists'%(email), status=200, mimetype='application/json')
+    else:
+        return jsonify({'User': User.get_all_users()})
+
+@app.route('/user/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def get_put_delete_user(id): 
+    if request.method == 'DELETE':
+        User.delete_user(id)
+        return Response("User deleted", 201, mimetype='application/json')
+    elif request.method == 'GET': 
+        return jsonify(User.get_user(id))
+
+    elif request.method == 'PUT':
+        email = request.form['email']
+        user = User.update_user(id, email)
+
+        return Response(jsonify(user), 200, mimetype='application/json')
+
 
 @app.route('/python', methods=['GET'])
 def index():
