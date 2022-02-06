0796a0a5c70b4979581fa210c8355064abe1dd1f
Kevin <kevin_mendez30@outlook.com>
2022-02-05 18:07:50 -0600

    feat: 🎸 (Consumer) Added docker and hotreload with nodemon

--- consumer-worker/index.js
+++ consumer-worker/index.js
@@ -1,17 +1,25 @@
 import Kafka from 'node-rdkafka';
 
-const consumer = new Kafka.KafkaConsumer({
-  'group.id': 'kafka',
-  'metadata.broker.list': 'localhost:9092',
-}, {});
+const consumer = new Kafka.KafkaConsumer(
+  {
+    'group.id': 'kafka',
+    'metadata.broker.list': 'localhost:9092'
+  },
+  {}
+);
 
 consumer.connect();
 
-consumer.on('ready', () => {
-  console.log('consumer ready..')
-  consumer.subscribe(['test']);
-  consumer.consume();
-}).on('data', function(data) {
-  console.log(`received message: ${data.value}`);
-  // console.log(`received message: ${objectToJson.fromBuffer(data.value)}`);
-});
+consumer
+  .on('ready', () => {
+    console.log('consumer ready..');
+    consumer.subscribe(['test']);
+    consumer.consume();
+  })
+  .on('data', function (data) {
+    console.log(`received message: ${data.value}`);
+    // console.log(`received message: ${objectToJson.fromBuffer(data.value)}`);
+  })
+  .on('error', function (err) {
+    console.log(err);
+  });
