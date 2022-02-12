6c9bcac413663edd19105631fd34fff4c9190e1a
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2022-02-11 21:43:02 -0600

    feat: 🎸 (Producer) Connect fastify with docker network (#4)

--- producer-api/index.js
+++ producer-api/index.js
@@ -2,17 +2,19 @@ import Fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
 import { routers } from './routers/index.js';
 
+const hostname = '0.0.0.0';
+const port = 3000;
+
 const fastify = Fastify({
   logger: true
 });
 
 fastify.register(fastifyCors, {});
-
 fastify.register(routers);
 
 const start = async () => {
   try {
-    await fastify.listen(3006);
+    await fastify.listen(port, hostname);
   } catch (err) {
     process.exit(1);
   }
